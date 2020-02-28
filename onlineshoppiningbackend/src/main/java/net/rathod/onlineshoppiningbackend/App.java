fcbfa94897b74f2769d86f0fa6d52657f3d0ab47
santhu <santhu@gmail.com>
2020-02-28 17:34:29 +0530

    I Have commited on 28-02-2020

---
+++ onlineshoppiningbackend/src/main/java/net/rathod/onlineshoppiningbackend/App.java
@@ -0,0 +1,13 @@
+package net.rathod.onlineshoppiningbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
